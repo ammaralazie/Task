66cea512a46fcca36cac8255088ad07e417161cb
ammar alazii <alaziiammar@gmail.com>
2021-11-15 01:55:36 +0300

    code

--- public/js/login.js
+++ public/js/login.js
@@ -10,14 +10,34 @@ function captchaVerifier() {
     "recaptcha-container"
   );
   recaptchaVerifier.render();
-}/* end of captchaVerifier */
+} /* end of captchaVerifier */
 
 const sendButton = document.getElementById("send");
 sendButton.addEventListener("click", (e) => {
   e.preventDefault();
   const appVerifier = window.recaptchaVerifier;
   const phoneNumber = document.getElementById("phone");
-  firebase.auth().signInWithPhoneNumber(phoneNumber.value, appVerifier)
+
+  let phon = "";
+  let nPhone = "";
+  if (!isNaN(phoneNumber.value)) {
+    for (var i = 0; i < phoneNumber.value.length; i++) {
+      if (phoneNumber.value[0] == "0" && phoneNumber.value.length ==11) {
+        if (i + 1 < phoneNumber.value.length) {
+          phon += phoneNumber.value[i + 1];
+        }
+      }else{
+        phon += phoneNumber.value[i];
+      } /* end of check 0 index */
+    } //end of for loop
+  } /* end of is nan */
+
+  nPhone = "+964 " + phon;
+  console.log(nPhone);
+
+  firebase
+    .auth()
+    .signInWithPhoneNumber(nPhone, appVerifier)
     .then((confirmationResult) => {
       // SMS sent. Prompt user to type the code from the message, then sign the
       // user in with confirmationResult.confirm(code).
@@ -35,27 +55,29 @@ sendButton.addEventListener("click", (e) => {
             console.log("user :", user.uid);
             //here we pass two var into header of axios
             axios.defaults.headers.common.token = user.ma;
-            axios.post("/login")
-            .then(res=>{if (res) {
-              
-              if(res){
-                window.location.href="/"
-              }
-            }})/* end of then */
-            .catch((err) => console.log(err));
+            axios
+              .post("/login")
+              .then((res) => {
+                if (res) {
+                  if (res) {
+                    window.location.href = "/";
+                  }
+                }
+              }) /* end of then */
+              .catch((err) => console.log(err));
             // ...
-          })/* end of catch */
+          }) /* end of catch */
           .catch((error) => {
             // User couldn't sign in (bad verification code?)
-            delete axios.defaults.headers.common.token
-            delete axios.defaults.headers.common.uid
+            delete axios.defaults.headers.common.token;
+            delete axios.defaults.headers.common.uid;
             /* axios.get("/")
             .then(res=>{if (res) {
               console.log(res);
             }}) */
             console.log("error :", error);
           });
-      });/* end of listener to verify*/
+      }); /* end of listener to verify*/
 
       //window.confirmationResult = confirmationResult;
       // ...
@@ -65,4 +87,4 @@ sendButton.addEventListener("click", (e) => {
       // ...
       console.log(error);
     });
-});/* end event listener of sendButton */
+}); /* end event listener of sendButton */
